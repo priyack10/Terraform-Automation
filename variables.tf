bd502a654010814ff3174f783e6f8938830fa1a3
priyack10 <116988785+priyack10@users.noreply.github.com>
2022-12-14 19:48:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "elb" 
+    default     =  "Microsoft" 
 }
 
 variable "instance_type" { 
